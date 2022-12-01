d96462b93337440464d6a1b0b7828c3d0c804524
danil-nizamov <danil.nizamov+github.codesignal.com>
2022-12-01 20:33:04 +0300

    added Comment and Video models

--- youtube/models.py
+++ youtube/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Video(models.Model):
+    title = models.CharField(max_length=30)
+    description = models.TextField()
+    preview = models.ImageField(upload_to="media/")
+    video = models.FileField(upload_to="media/")
+    upload_datetime = models.DateTimeField(auto_now_add=True)
+    likes = models.IntegerField(default=0)
+    dislikes = models.IntegerField(default=0)
+    channel = models.OneToOneField(User, on_delete=models.PROTECT)
+
+
+class Comment(models.Model):
+    user = models.OneToOneField(User, on_delete=models.PROTECT)
+    text = models.TextField()
+    datetime = models.DateTimeField(auto_now_add=True)
